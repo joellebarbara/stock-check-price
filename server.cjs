408c79986b565b5c65965166ee4f4cadfae7a2e7
Joelle Barbara <132681203+joellebarbara@users.noreply.github.com>
2024-02-11 17:36:23 +0200

    Update server.cjs

--- server.cjs
+++ server.cjs
@@ -1,68 +1,62 @@
-'use strict';
+// server.cjs
 import 'dotenv/config';
-
-const express     = require('express');
-const bodyParser  = require('body-parser');
-const cors        = require('cors');
-
-const apiRoutes         = require('./routes/api.js');
-const fccTestingRoutes  = require('./routes/fcctesting.js');
-const runner            = require('./test-runner');
-require("./db-connection")
-
-let helmet = require("helmet")
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import helmet from 'helmet';
+import apiRoutes from './routes/api.js';
+import fccTestingRoutes from './routes/fcctesting.js';
+import runner from './test-runner';
+import './db-connection';
 
 const app = express();
 
 app.use('/public', express.static(process.cwd() + '/public'));
-
-app.use(cors({origin: '*'})); //For FCC testing purposes only
-
+app.use(cors({ origin: '*' })); // For FCC testing purposes only
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-//content security policies
-app.use(helmet.contentSecurityPolicy({
-  directives : {
-    scriptSrc : ["'self'"],
-    styleSrc : ["'self'"]
-  }
-}))
+// content security policies
+app.use(
+  helmet.contentSecurityPolicy({
+    directives: {
+      scriptSrc: ["'self'"],
+      styleSrc: ["'self'"],
+    },
+  })
+);
 
-//Index page (static HTML)
-app.route('/')
-  .get(function (req, res) {
-    res.sendFile(process.cwd() + '/views/index.html');
-  });
-
-//For FCC testing purposes
-fccTestingRoutes(app);
-
-//Routing for API 
-apiRoutes(app);  
-    
-//404 Not Found Middleware
-app.use(function(req, res, next) {
-  res.status(404)
-    .type('text')
-    .send('Not Found');
+// Index page (static HTML)
+app.route('/').get((req, res) => {
+  res.sendFile(process.cwd() + '/views/index.html');
 });
 
-//Start our server and tests!
-app.listen(process.env.PORT || 3000, function () {
-  console.log("Listening on port " + process.env.PORT);
-  if(process.env.NODE_ENV==='test') {
+// For FCC testing purposes
+fccTestingRoutes(app);
+
+// Routing for API
+apiRoutes(app);
+
+// 404 Not Found Middleware
+app.use((req, res, next) => {
+  res.status(404).type('text').send('Not Found');
+});
+
+// Start our server and tests!
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`Listening on port ${process.env.PORT || 3000}`);
+  if (process.env.NODE_ENV === 'test') {
     console.log('Running Tests...');
-    setTimeout(function () {
+    setTimeout(() => {
       try {
         runner.run();
-      } catch(e) {
-        var error = e;
-          console.log('Tests are not valid:');
-          console.log(error);
+      } catch (e) {
+        const error = e;
+        console.log('Tests are not valid:');
+        console.log(error);
       }
     }, 3500);
   }
 });
 
-module.exports = app; //for testing
+export default app; // for testing

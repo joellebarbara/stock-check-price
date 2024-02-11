3bb0f5f1f2df9a5be3eb52a965e332d511aab71f
Joelle Barbara <132681203+joellebarbara@users.noreply.github.com>
2024-02-11 17:25:03 +0200

    Update api.js

--- routes/api.js
+++ routes/api.js
@@ -1,6 +1,6 @@
 'use strict';
 const StockModel = require("../models").Stock;
-const fetch = require('node-fetch');
+import fetch from 'node-fetch';
 const cors = require('cors');
 
 async function createStock(stock, like, ip) {
